5fc2a989fda3c882115a91191218738f2490ca3a
Subzy132 <subhaanh00@gmail.com>
2022-10-11 11:25:40 +0100

    changes to readme file and added a new tutorial py file

---
+++ operators.py
@@ -0,0 +1,21 @@
+# Let's see the operators in action
+
+### Intro to Data types & Operators
+
+# - `+ - * / `
+
+##### comparison Operators
+#
+# - `>` greater than
+# - `<` lesser than
+# - `==` True or False
+# - `>=` greater than or equals to
+# - `<=` Lesser than or equals to
+
+a = 24
+b = 15
+
+print(a + b) # outcome of a and b added = 39
+print(a - b) # outcome of a - b = 9
+print(a > b) # outcome boolean = True
+print(a == b) # outcome false because a does not equal b
